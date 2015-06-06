b1b3d1783857f583714891af8d0666427bc486a4
Trey Robinson <rdrobinson3@gmail.com>
2015-06-06 09:38:20 -0400

    Initial commit. Base Google example.

---
+++ app/src/main/java/io/mindmine/databindingdemo/User.java
@@ -0,0 +1,14 @@
+package io.mindmine.databindingdemo;
+
+/**
+ * Created by Trey Robinson on 6/6/15.
+ * Copyright 2015 MindMine LLC.
+ */
+public class User {
+    public final String firstName;
+    public final String lastName;
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
